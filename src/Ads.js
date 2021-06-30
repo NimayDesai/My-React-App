9a59b144223bb032e270260b1926f96ac5657712
Nimay Desai <robtop.test.gd@gmail.com>
2021-06-30 16:50:34 -0400

    Add Ads for a joke

---
+++ src/Ads.js
@@ -0,0 +1,18 @@
+const Ads = () => {
+    return (
+        <div className="ads">
+            <h1>Free Cars Free Get your free car today!!!!1! 🤑🤑🤑</h1>
+            <h1>You could get this </h1>
+            <h1>                   |</h1>
+            <h1>                   |</h1>
+            <h1>                   |</h1>
+            <h1>                   v</h1>
+            <a href="/free-cars">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/5/5d/401_Gridlock.jpg" alt="Free Car" />
+            </a>
+
+        </div>
+    )
+}
+
+export default Ads;
