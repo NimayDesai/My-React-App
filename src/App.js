5356a9c8c1cd3ac474331a64a6f23f54f2848c8b
Nimay Desai <robtop.test.gd@gmail.com>
2021-07-08 14:06:36 -0400

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Privacy from './Privacy';
+
 
 function App() {
     return (
+        <Router>
         <div className="App">
             <Navbar />
             <div className="content">
-                <Home />
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/privacy">
+                        <Privacy />
+                    </Route>
+                </Switch>
             </div>
         </div>
+        </Router>
+
     );
 }
 
