b79a8347a7555139139d2bd2bd30b3565af7b797
Nimay Desai <ninudasss@protonmail.com>
2021-07-17 17:32:54 -0400

    Add Create.js

--- src/App.js
+++ src/App.js
@@ -5,28 +5,32 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Privacy from './Privacy';
 import Help from './Help';
 import NotFound from './NotFound';
+import Create from './Create';
 
 function App() {
     return (
         <Router>
-        <div className="App">
-            <Navbar />
-            <div className="content">
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Route path="/privacy">
-                        <Privacy />
-                    </Route>
-                    <Route path="/help">
-                        <Help />
-                    </Route>
-                    <Route path="*">
-                        <NotFound />
-                    </Route>
-                </Switch>
-            </div>
+            <div className="App">
+                <Navbar />
+                <div className="content">
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route path="/privacy">
+                            <Privacy />
+                        </Route>
+                        <Route path="/help">
+                            <Help />
+                        </Route>
+                        <Route path="/create">
+                            <Create />
+                        </Route>
+                        <Route path="*">
+                            <NotFound />
+                        </Route>
+                    </Switch>
+                </div>
         </div>
         </Router>
 
